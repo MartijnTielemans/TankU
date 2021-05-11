d9dcc275f63b3211a63082c489789fbd4552c6e5
Martijn <martijn.tielemans@live.nl>
2021-05-11 15:41:51 +0200

    Fixed lerping

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour, IInteractable
 {
+    float startPoint;
+    public float endPoint;
+
+    [Range(0, 1)]
+    public float interpolation;
+
+    [Range(0, 1)]
+    public float interpolSpeed;
+    public bool moveUp = false;
+    public bool moveDown = false;
+
+    private void Start()
+    {
+        startPoint = transform.position.y;
+    }
+
+    private void Update()
+    {
+        //if (!moveUp && !moveDown)
+        //{
+
+        //}
+        //else if (moveUp)
+        //{
+        //    transform.position = new Vector3(transform.position.x, Mathf.Lerp(startPoint, endPoint, interpolation), transform.position.z);
+
+        //    if (interpolation !>= 1)
+        //    interpolation += interpolSpeed * Time.deltaTime;
+        //}
+        //else if (moveDown)
+        //{
+        //    transform.position = new Vector3(transform.position.x, Mathf.Lerp(endPoint, startPoint, interpolation), transform.position.z);
+
+        //    if (interpolation !>= 1)
+        //        interpolation += interpolSpeed * Time.deltaTime;
+        //}
+        if (moveUp && interpolation <= 1)
+        {
+            interpolation += interpolSpeed * Time.deltaTime;
+        }
+        if (moveDown && interpolation >= 0)
+        {
+            interpolation -= interpolSpeed * Time.deltaTime;
+        }
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(startPoint, endPoint, interpolation), transform.position.z);
+    }
+
     void IInteractable.Action(string type)
     {
         if (type == "on")
         {
-            Debug.Log("Iton");
+            moveUp = true;
+            moveDown = false;
         }
         else if (type == "off")
         {
-
+            moveDown = true;
+            moveUp = false;
         }
     }
 }
